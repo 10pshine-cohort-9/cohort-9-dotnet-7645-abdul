4e60e62f1b976318c43d5c433c84fb2c52467cf0
Abdul Moiz <abdulmoiz22607@gmail.com>
2026-08-13 00:54:39 +0500

    task management crud

--- backend/TaskManagement.API/TaskManagement.Domain/Entities/TaskCategory.cs
+++ backend/TaskManagement.API/TaskManagement.Domain/Entities/TaskCategory.cs
@@ -15,8 +15,7 @@ public class TaskCategory
 
     public DateTime? UpdatedAt { get; set; }
 
-    // Navigation property
-
+    // Navigation properties
     public ICollection<TaskItem> TaskItems { get; set; }
         = new List<TaskItem>();
 }
